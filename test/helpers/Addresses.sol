da9605f3c10b255a4216f57d347d7ac63433ada8
tavanam rama <rcrtavanam@gmail.com>
2026-08-20 16:32:34 +0530

    test(fork): mainnet protocol addresses

---
+++ test/helpers/Addresses.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library Mainnet {
+    address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+    address internal constant USDS = 0xdC035D45d973E3EC169d2276DDab16f1e407384F;
+    address internal constant USDE = 0x4c9EDD5852cd905f086C759E8383e09bff1E68B3;
+
+    address internal constant AAVE_V3_POOL = 0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2;
+    address internal constant AAVE_A_USDC = 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c;
+    address internal constant AAVE_A_USDT = 0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a;
+    address internal constant AAVE_A_DAI = 0x018008bfb33d285247A21d44E50697654f754e63;
+    address internal constant AAVE_A_WETH = 0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8;
+    address internal constant AAVE_A_WBTC = 0x5Ee5bf7ae06D1Be5997A1A72006FE6C607eC6DE8;
+    address internal constant AAVE_REWARDS_CONTROLLER = 0x8164Cc65827dcFe994AB23944CBC90e0aa80bFcb;
+
+    address internal constant SPARK_POOL = 0xC13e21B648A5Ee794902342038FF3aDAB66BE987;
+    address internal constant SPARK_SP_DAI = 0x4DEDf26112B3Ec8eC46e7E31EA5e123490B05B8B;
+
+    address internal constant COMET_USDC = 0xc3d688B66703497DAA19211EEdff47f25384cdc3;
+    address internal constant COMET_USDT = 0x3Afdc9BCA9213A35503b077a6072F3D0d5AB0840;
+    address internal constant COMET_WETH = 0xA17581A9E3356d9A858b789D68B4d866e593aE94;
+    address internal constant COMET_REWARDS = 0x1B0e765F6224C21223AeA2af16c1C46E38885a40;
+    address internal constant COMP = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
+
+    address internal constant SDAI = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
+    address internal constant SUSDS = 0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD;
+    address internal constant SUSDE = 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497;
+
+    address internal constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address internal constant RETH = 0xae78736Cd615f374D3085123A210448E74Fc6393;
+    address internal constant SFRXETH = 0xac3E018457B222d93114458476f3E3416Abbe38F;
+
+    address internal constant CURVE_3POOL = 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
+    address internal constant CURVE_3POOL_LP = 0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490;
+    address internal constant CONVEX_BOOSTER = 0xF403C135812408BFbE8713b5A23a04b3D48AAE31;
+    address internal constant CONVEX_3POOL_REWARDS = 0x689440f2Ff927E1f24c72F1087E1FAF471eCe1c8;
+    uint256 internal constant CONVEX_3POOL_PID = 9;
+    address internal constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+    address internal constant CVX = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
+
+    address internal constant CHAINLINK_USDC_USD = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+    address internal constant CHAINLINK_USDT_USD = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
+    address internal constant CHAINLINK_DAI_USD = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+    address internal constant CHAINLINK_ETH_USD = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+    address internal constant CHAINLINK_BTC_USD = 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c;
+
+    address internal constant UNISWAP_V3_ROUTER = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+}
